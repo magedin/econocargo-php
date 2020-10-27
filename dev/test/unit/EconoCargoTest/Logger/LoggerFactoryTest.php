3a31487020034d3e3573a073476447821dd1178e
Tiago Sampaio <tiago@tiagosampaio.com>
2020-10-27 15:58:59 -0300

    Improvements.

---
+++ dev/test/unit/EconoCargoTest/Logger/LoggerFactoryTest.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace EconoCargoTest\Logger;
+
+use EconoCargo\Logger\LoggerFactory;
+use EconoCargoTest\TestCase;
+
+class LoggerFactoryTest extends TestCase
+{
+    /**
+     * @var LoggerFactory
+     */
+    private $object;
+
+    protected function setUp() : void
+    {
+        $this->object = $this->createObject(LoggerFactory::class);
+    }
+
+    /**
+     * @test
+     */
+    public function getLogger()
+    {
+        $this->assertInstanceOf(
+            \Psr\Log\LoggerInterface::class,
+            $this->object->getLogger('debug', 'debug.log')
+        );
+    }
+}
---
+++ src/ApiFactory.php
@@ -0,0 +1,88 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace EconoCargo;
+
+use EconoCargo\Framework\DI\ContainerRepository;
+
+/**
+ * Class ApiFactory
+ */
+class ApiFactory
+{
+
+    /**
+     * @var \DI\Container
+     */
+    private static $container;
+
+    /**
+     * @var array
+     */
+    public static $config = [
+        'definitions' => DIR_DI_CONFIG
+    ];
+
+    /**
+     * @var array
+     */
+    private static $customConfig = [];
+
+    /**
+     * @param string $token
+     * @param array  $config
+     *
+     * @return ApiInterface
+     * @throws \DI\DependencyException
+     * @throws \DI\NotFoundException
+     */
+    public static function create(string $token, array $config = [])
+    {
+        if (!empty($config)) {
+            /** If there's a customized configuration the application can load it. */
+            self::$customConfig = (array) $config;
+        }
+
+        self::setupContainer();
+
+        $api = self::createApiInstance($token);
+
+        self::$container->set(Api::class, $api);
+
+        return $api;
+    }
+
+    /**
+     * @param string $token
+     *
+     * @return ApiInterface
+     * @throws \DI\DependencyException
+     * @throws \DI\NotFoundException
+     */
+    private static function createApiInstance(string $token)
+    {
+        return self::$container->make(ApiInterface::class, [
+            'token' => $token,
+        ]);
+    }
+
+    /**
+     * Setup the container.
+     *
+     * @throws \EconoCargo\Framework\Exception\ExceptionInterface
+     */
+    private static function setupContainer()
+    {
+        self::$container = ContainerRepository::getInstance(self::getConfig());
+    }
+
+    /**
+     * @return array
+     */
+    private static function getConfig()
+    {
+        $config = array_merge_recursive(self::$config, self::$customConfig);
+        return $config;
+    }
+}
